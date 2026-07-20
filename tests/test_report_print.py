4d0f391fa7f858f2b0b9235a78502e3d96692eab
Laraib <lxh417bham@gmail.com>
2026-07-20 13:58:43 +0100

    feat(report): print stylesheet for clean PDF export

---
+++ tests/test_report_print.py
@@ -0,0 +1,10 @@
+from cybergraph.visualize import _read_asset
+
+
+def test_print_media_block_hides_chrome():
+    css = _read_asset("report/report.css")
+    idx = css.find("@media print")
+    assert idx != -1
+    block = css[idx:]
+    assert "#cg-nav" in block and "display: none" in block
+    assert ".toolbar" in block
